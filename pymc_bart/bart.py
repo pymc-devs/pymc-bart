3235f50bd60d1f3cab44f20c0b012274d8180fb0
aloctavodia <aloctavodia@gmail.com>
2022-12-22 09:08:49 -0300

    fix shape

--- pymc_bart/bart.py
+++ pymc_bart/bart.py
@@ -22,7 +22,6 @@ from pymc.distributions.distribution import Distribution, _moment
 from pymc.logprob.abstract import _logprob
 import pytensor.tensor as pt
 from pytensor.tensor.random.op import RandomVariable
-from pytensor.tensor.var import Variable
 
 
 from .utils import _sample_posterior
@@ -41,11 +40,7 @@ class BARTRV(RandomVariable):
     all_trees = None
 
     def _supp_shape_from_params(self, dist_params, rep_param_idx=1, param_shapes=None):
-        if isinstance(self.X, Variable):
-            shape = self.X.shape[0].eval()
-        else:
-            shape = self.X.shape[0]
-        return (shape,)
+        return dist_params[0].shape[:1]
 
     @classmethod
     def rng_fn(cls, rng=None, X=None, Y=None, m=None, alpha=None, split_prior=None, size=None):
