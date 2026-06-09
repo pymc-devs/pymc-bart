2ca26e762fe435b243d88e6c831557009b07adb7
OttoVintola <otto.e.vintola@gmail.com>
2026-06-09 16:23:04 +0300

    update workflow

--- pymc_bart/__init__.py
+++ pymc_bart/__init__.py
@@ -11,7 +11,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import bartrs  # registers PGBART with PyMC
+import bartrs  # registers PGBART with PyMC. If removed, the PGBART step method will not be detected by PyMC.
 
 from pymc_bart.bart import BART
 from pymc_bart.utils import (
